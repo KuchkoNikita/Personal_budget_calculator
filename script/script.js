59ce8939da351bb4b6d27131f50026f634a7fb05
Nikita Kuchko <kuchko.n.study@gmail.com>
2020-01-25 18:04:11 +0300

    Add JS and CSS

---
+++ script/script.js
@@ -0,0 +1,9 @@
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission; 
+let period;
+
+alert('Lorem ipsum dolor sit amet, consectetuer adipiscing elit.');
+console.log('Hellow World!');
